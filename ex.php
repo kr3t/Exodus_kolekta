6df2140e97a6c4f2c0a96d48efb7cc1552f61bbd
Krzysztof Kreft <krzysztof.kreft@gmail.com>
2018-01-18 18:14:37 +0100

    s
    s

---
+++ ex.php
@@ -0,0 +1,4 @@
+<?php
+$ile = $_POST['ile'];
+header("Location: https://ssl.dotpay.pl/test_payment/?id=726756&kwota=$ile&opis=Dawrowizna%20na%20cele%20kultu%20religijnego"); //przekierowanie na adres zdalny
+?>
