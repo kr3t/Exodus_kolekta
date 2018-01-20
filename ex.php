a98320080f996065454043bd87de9dc7caa623df
Krzysztof Kreft <krzysztof.kreft@gmail.com>
2018-01-20 16:26:45 +0100

    kkreft
    Add ThankYou page

--- ex.php
+++ ex.php
@@ -1,4 +1,4 @@
 <?php
 $ile = $_POST['ile'];
-header("Location: https://ssl.dotpay.pl/test_payment/?id=726756&kwota=$ile&opis=Dawrowizna%20na%20cele%20kultu%20religijnego"); //przekierowanie na adres zdalny
+header("Location: https://ssl.dotpay.pl/test_payment/?id=726756&kwota=$ile&opis=Dawrowizna%20na%20cele%20kultu%20religijnego&type=4&URL=http://127.0.0.1/ex2/exodus_kolekta/ty.php&personal_data=1&bylaw=1"); //przekierowanie na adres zdalny
 ?>
---
+++ ty.php
@@ -0,0 +1,46 @@
+<html lang="pl">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <title>Kolekta</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    .mont{font-family: 'Montserrat', sans-serif;
+}    
+    </style>   
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="main.css" rel="stylesheet">
+     
+
+    
+</head>
+<body>
+   <div class="onediv"><img src="logo.png" width="100%"></div>
+
+<?php
+$ile = $_GET['status'];    
+if ($ile == "OK"){
+?><div class="ty_text">
+<span class="thanks mont">Dziękujemy za twoją chojność</span>
+    </div>   
+<?php    
+}
+else{
+?>
+
+<div class="ty_text">
+<span class="onemore mont">Coś poszło nie tak... sprobuj jeszcze raz</span>
+    </div>  
+   
+<?php    
+}    
+    
+?> 
+<div class="ty_text">   
+<span>Za chwile przenieśmiemy Ciebie na stronę kolekty.</span>
+</div>
+
+<meta http-equiv="refresh" content="8; url=index.html">
+</body>
+</html>
